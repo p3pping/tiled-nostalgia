ef7b52e83ae2693f5c1be3f6a9bda9def13a1eb6
P3pping <mikepepping@gmail.com>
2020-09-13 11:12:54 +1000

    Changed procs to lambdas.

--- src/main.rb
+++ src/main.rb
@@ -1,11 +1,11 @@
 require_relative '../lib/windows/window.rb'
 
 
-window = Window.new(800, 600, caption: "Hello world")
+window = Window.new(800, 600, caption: "Tiled Nostalgia")
 
-window.update = Proc.new { |delta| puts "FPS: #{1.0/delta}" }
-window.on_input_pressed = Proc.new { |input_id| puts "Pressed: #{input_id}" }
-window.on_input_released = Proc.new { |input_id| puts "Released: #{input_id}" }
-window.on_close = Proc.new { puts "Closing..." }
+window.update = ->(delta) { puts "FPS: #{1.0/delta}" }
+window.on_input_pressed   = ->(input_id) { puts "Pressed: #{input_id}" }
+window.on_input_released  = ->(input_id) { puts "Released: #{input_id}" }
+window.on_close = -> { puts "Closing..." }
 
 window.show
