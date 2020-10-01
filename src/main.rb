7daec43b4abec642beea35e90625284469a8477f
Michael Pepping <michael.pepping@pieronline.org>
2020-10-01 10:19:18 +1000

    Removed redundant context passed to factories.

--- src/main.rb
+++ src/main.rb
@@ -3,21 +3,20 @@ require_relative '../lib/factories/factories.rb'
 require_relative '../lib/utilities/inputs.rb'
 
 context = Rendering::Context.new(800, 600, 5)
-window  = Factories::Window.create("Tiled Nostalgia", context)
+window  = Factories::Window.create('Tiled Nostalgia', context)
 
-tileset = Factories::Tileset.create(16, 16, './assets/default_tileset.png', context)
-map     = Factories::Map.create('assets/map.csv', tileset, context)
-sprite  = Factories::Sprite.create('./assets/default_sprite.png', 16, 16, context)
-
+tileset = Factories::Tileset.create(16, 16, './assets/default_tileset.png')
+map     = Factories::Map.create('assets/map.csv', tileset)
+sprite  = Factories::Sprite.create('./assets/default_sprite.png', 16, 16)
 
 window.on_input_pressed = Proc.new do |input_id|
   case input_id
-    when INPUTS::W_KEY
-      sprite.change_position(sprite.position + Vector2D.new(0,-1))
-    when INPUTS::S_KEY
-      sprite.change_position(sprite.position + Vector2D.new(0,1))
-    when INPUTS::ESC_KEY
-      window.close!
+  when INPUTS::W_KEY
+    sprite.change_position(sprite.position + Vector2D.new(0, -1))
+  when INPUTS::S_KEY
+    sprite.change_position(sprite.position + Vector2D.new(0, 1))
+  when INPUTS::ESC_KEY
+    window.close!
   end
 
   puts sprite.position.y
